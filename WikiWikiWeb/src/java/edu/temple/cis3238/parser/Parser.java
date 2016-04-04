5b7a1ccb93b1eabcb5f386dc792241af383934cc
cap9090 <cprajzner@temple.edu>
2016-04-04 19:23:16 -0400

    the parser class now contains a ParseAndCategorize method which gives array lists of the topics and tags

--- WikiWikiWeb/src/java/edu/temple/cis3238/parser/Parser.java
+++ WikiWikiWeb/src/java/edu/temple/cis3238/parser/Parser.java
@@ -10,7 +10,7 @@ package edu.temple.cis3238.parser;
  * @author CAP
  */
 import java.util.Scanner;
-import java.lang.StringBuilder;
+import java.util.ArrayList;
 
 public class Parser{
     
@@ -75,4 +75,47 @@ public class Parser{
       
       return result.toString();
   }
+  
+  /*returns an arrary of arrayLists, the ArrayList at index 0 contains an array list of 
+    topics and the ArrayList at index 1 contains an array list of tags
+  */
+  public static ArrayList<String> [] parseAndCategorize(String inputWikiText){
+
+      
+      ArrayList<String> [] topicsNtags = new ArrayList [2];
+      topicsNtags [0] = new ArrayList<>();
+      topicsNtags [1] = new ArrayList<>();
+      
+      
+      Scanner scanner = new Scanner(inputWikiText);
+            
+      String s = null;
+      int end = 0;
+      
+      while(scanner.hasNext()){
+          
+          s = scanner.next();
+
+          if (s.substring(0, 2).equals(WikiMarkup.FRONT_TOPIC.toString())){
+              
+              end = s.indexOf(WikiMarkup.BACK_TOPIC.toString());
+              topicsNtags[0].add(s.substring(2,end));
+
+          }
+          else if (s.substring(0, 2).equals(WikiMarkup.FRONT_TAG.toString())){
+
+              end = s.indexOf(WikiMarkup.BACK_TAG.toString());
+              topicsNtags[1].add(s.substring(2,end));
+
+          }
+           //add more else if for more regexes
+          else{
+              //do nothing ..this only use for annotate method...but keep for logic's sake
+          }
+     
+      }  
+      
+      return topicsNtags;
+  }
+
 }
