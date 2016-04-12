9b69ebf8f192dffa9384b427a228da22a1069916
ThoughtProvoking <tuf13673@temple.edu>
2016-04-12 12:47:29 -0400

    Finished topic and tag buttons; can get and modify selected text; still need to insert back into editor

--- WikiWikiWeb/web/editor.js
+++ WikiWikiWeb/web/editor.js
@@ -1,62 +1,88 @@
-$(document).ready(function() {
+$(document).ready(function () {
     $("#editor").jqxEditor({
         width: "100%",
+        tools: 'bold italic underline | format font size | color background | left \\n\
+                center right | outdent indent | ul ol | image | link | html | clean | \\n\
+                topic tag',
         createCommand: function (name) {
             switch (name) {
-                case 'topic link':
+                case 'topic':
                     return {
                         type: 'button',
                         tooltip: 'Topic Link',
                         init: function (widget) {
-                            widget.jqxButton({height: 25, width: 40});
-                            widget.html("<span style='line-height: 23px;'>Topic Link</span>");
+                            widget.jqxButton({height: 25, width: 20});
+                            widget.html("<span style='line-height: 23px;'>[]</span>");
                         },
                         refresh: function (widget, style) {
                             // go here when selection changes
                         },
                         action: function (widget, editor) {
                             // action of command
-                            editor.jqxEditor('print');
+                            markUp('topic');
                         }
                     };
-//                case 'tag link':
+                case 'tag':
+                    return {
+                        type: 'button',
+                        tooltip: 'Tag Link',
+                        init: function (widget) {
+                            widget.jqxButton({height: 25, width: 20});
+                            widget.html("<span style='line-height: 23px;'>{}</span>");
+                        },
+                        refresh: function (widget, style) {
+                            // go here when selection changes
+                        },
+                        action: function (widget, editor) {
+                            // action of command
+                            markUp('tag');
+                        }
+                    };
             }
         }
     });
 });
 
 function markUp(type) {
-    // get editor textarea object
-    var editor = $('#editor');
-    // get selected text, length of entire text, start and end index of selected text
-    var text = window.getSelection();
-    var len = editor.val().length;
-    var start = editor[0].selectionStart;
-    var end = editor[0].selectionEnd;
-
-    // modify text depending on the type of markUp
+    // get hightlighted text and store into selectedText
+    var iframe = $('#editor').find('iframe')[0],
+            iframeWindow = iframe.contentWindow || iframe,
+            iframeDocument = iframe.contentDocument || iframe.contentWindow.document,
+            selectedText = "";
+    
+    if (iframeWindow.getSelection) {
+        selectedText = iframeWindow.getSelection().toString();
+    } else if (iframeDocument.selection && iframeDocument.selection.type != "Control") {
+        selectedText = iframeDocument.selection.createRange().text;
+    }
+//    var len = editor.val().length;
+//    var start = editor.val()[0].selectionStart;
+//    var end = editor.val()[0].selectionEnd;
+    
+//    // modify text depending on the type of markUp
     switch (type) {
-        case 'boldlink':    // bold link
-            text = ('[[').concat(text).concat(']]');
+        case 'topic':    // bold link
+            selectedText = ('[[').concat(selectedText).concat(']]');
             break;
-        case 'link':        // regular link
-            text = ('{{').concat(text).concat('}}');
+        case 'tag':        // regular link
+            selectedText = ('{{').concat(selectedText).concat('}}');
             break;
         default:
             break;
-    }
+    }alert($('#editor')[0]);
 
-    // replace selected text with markup text
-    editor.val(editor.val().substring(0, start) + text + editor.val().substring(end, len));
+//    // replace selected text with markup text
+//    editor.val(editor.val().substring(0, start) + text + editor.val().substring(end, len));
 }
 
 // allows for wiki markup shortcut keys
-$(document).on('keydown', '#editor', function (e) {
-    if (e.ctrlKey && e.keyCode === 76) {
-        if (e.shiftKey) {
-            markUp('boldlink'); // bold hyperlink
-        } else {
-            markUp('link');     // regular hyperlink
-        }
-    }
-});
+// DOES NOT WORK ANYMORE
+//$(document).on('keydown', $('#editor'), function (e) {
+//    if (e.ctrlKey && e.keyCode === 76) {
+//        if (e.shiftKey) {
+//            markUp('boldlink'); // bold hyperlink
+//        } else {
+//            markUp('link');     // regular hyperlink
+//        }
+//    }
+//});
